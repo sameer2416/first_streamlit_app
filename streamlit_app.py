b7face87cec175c936c9f5cbeee85358a1ede98e
Sameer <99891973+sameer2416@users.noreply.github.com>
2022-06-23 14:32:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.dataframe(my_data_rows)
 
 #New section to display fruityvice api response
 add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
 
